25893e051ef0b7d50a16046fd0f35574fbf78c61
Frazer McLean <frazer@frazermclean.co.uk>
2020-07-11 18:18:23 +0200

    Remove support for `python setup.py test`

--- setup.py
+++ setup.py
@@ -1,8 +1,6 @@
 import re
-import sys
 
 from setuptools import setup, find_packages
-from setuptools.command.test import test as TestCommand
 
 
 INIT_FILE = 'sqlalchemy_aio/__init__.py'
@@ -39,25 +37,6 @@ extras_require['trio'] = [
 ]
 
 
-class PyTest(TestCommand):
-    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
-
-    def initialize_options(self):
-        TestCommand.initialize_options(self)
-        self.pytest_args = []
-
-    def finalize_options(self):
-        TestCommand.finalize_options(self)
-        self.test_args = []
-        self.test_suite = True
-
-    def run_tests(self):
-        # import here, cause outside the eggs aren't loaded
-        import pytest
-        errno = pytest.main(self.pytest_args)
-        sys.exit(errno)
-
-
 setup(
     name='sqlalchemy_aio',
     version=VERSION,
@@ -67,7 +46,6 @@ setup(
     author_email=EMAIL,
     url='https://github.com/RazerM/sqlalchemy_aio',
     packages=find_packages(exclude=['tests']),
-    cmdclass={'test': PyTest},
     classifiers=[
         'Development Status :: 4 - Beta',
         'License :: OSI Approved :: MIT License',
